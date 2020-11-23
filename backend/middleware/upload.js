fde83d11d31cf3fe7eddc32bb544c79d01930226
gxalmet <gxalmet@gmail.com>
2020-11-23 17:39:25 +0100

    commit

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -5,7 +5,8 @@ import dotenv from 'dotenv';
 
 const conf = dotenv.config();
 
-const MongoURI = conf.parsed.MONGODB_URL || process.env.MONGODB_URL;
+// const MongoURI = conf.parsed.MONGODB_URL || process.env.MONGODB_URL;
+const MongoURI = process.env.MONGODB_URL;
 
 var storage = new GridFsStorage({
     url: MongoURI,
